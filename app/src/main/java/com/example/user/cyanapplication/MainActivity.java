ffb98f675a6e00c2e2797388d19b2d15c53bc95a
cyan <fantasylnad@gmail.com>
2016-07-18 21:56:46 +0800

    temp

--- app/src/main/java/com/example/user/cyanapplication/MainActivity.java
+++ app/src/main/java/com/example/user/cyanapplication/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.user.cyanapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.util.Log;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    final static String DebugTag="MainActivity";
     @Override  //Override parent
     protected void onCreate(Bundle savedInstanceState) { //Constructor
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.new_layout);
+//        Log.d(DebugTag,"HAHAHA");
+//        //Log.i(DebugTag,"hehehe");
+//        Log.d(DebugTag,"WAWAWA");
+//        Toast.makeText(this, "This is a test", Toast.LENGTH_LONG).show();
     }
 }

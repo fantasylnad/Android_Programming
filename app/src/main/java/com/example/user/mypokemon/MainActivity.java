191ccec6a8891206af65ad299836f4ab4f4e58c9
cyan <fantasylnad@gmail.com>
2016-07-21 21:57:43 +0800

    update 1

--- app/src/main/java/com/example/user/mypokemon/MainActivity.java
+++ app/src/main/java/com/example/user/mypokemon/MainActivity.java
@@ -5,17 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView info;
     EditText name;
     RadioGroup options;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options = (RadioGroup) findViewById(R.id.radioGroup);
+        info = (TextView) findViewById(R.id.textView);
+        name = (EditText)  findViewById(R.id.Name);
 
         MyOnClickListener submitListener = new MyOnClickListener();
         Button submit= (Button)findViewById(R.id.submit);
@@ -29,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
             switch (viewId){
                 case R.id.submit:
                     // get pokemon
+//                    if (name != ""){
+//                        info.setText("你好, "+name);
+//                    }
                     break;
             }
         }
